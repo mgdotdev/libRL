8f72fcab6ef8565b92cf900eafb9e47555948377
Michael Green <1mikegrn@gmail.com>
2019-10-02 14:51:26 -0500

    finished CARL

--- example.py
+++ example.py
@@ -4,20 +4,16 @@ def main():
     file_location = r'D:\Research and Teaching\University of Missouri-Kansas City\Dr. Xiaobo Chen\Microwave Absorption\Data\(nBu4)2Mo6 {A}'
     file_name = r'\data1'
 
-    # res = libRL.RL(
-    #     Mcalc=file_location + file_name + '.csv',
-    #     f_set=(1,10,0.1), d_set=(0,5,1), interp='linear',
-    #     multiprocessing=0, multicolumn=True
-    # )
+    reflection_loss = libRL.RL(
+        Mcalc=file_location + file_name + '.csv',
+        f_set=(1,10,0.1), d_set=(0,5,1), interp='linear',
+        multiprocessing=0, multicolumn=True
+    )
 
-    res = libRL.CARL(
+    characterization = libRL.CARL(
         Mcalc=file_location + file_name + '.csv',
         f_set=(1,10,0.1), as_dataframe=True
     )
-
-    res.to_clipboard()
-
-    print(res)
 
 if __name__ == "__main__":
     main()
