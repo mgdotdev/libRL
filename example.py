e709d851abebd3b6dc3dc71b38c3ad8b034ad26e
Michael Green <1mikegrn@gmail.com>
2019-10-02 14:50:05 -0500

    finished CARL

--- example.py
+++ example.py
@@ -4,13 +4,20 @@ def main():
     file_location = r'D:\Research and Teaching\University of Missouri-Kansas City\Dr. Xiaobo Chen\Microwave Absorption\Data\(nBu4)2Mo6 {A}'
     file_name = r'\data1'
 
-    res = libRL.RL(
+    # res = libRL.RL(
+    #     Mcalc=file_location + file_name + '.csv',
+    #     f_set=(1,10,0.1), d_set=(0,5,1), interp='linear',
+    #     multiprocessing=0, multicolumn=True
+    # )
+
+    res = libRL.CARL(
         Mcalc=file_location + file_name + '.csv',
-        f_set=(1,10,0.1), d_set=(0,5,1), interp='linear',
-        multiprocessing=0, multicolumn=True
+        f_set=(1,10,0.1), as_dataframe=True
     )
 
-    print(res.shape)
+    res.to_clipboard()
+
+    print(res)
 
 if __name__ == "__main__":
     main()
--- libRL.py
+++ libRL.py
@@ -32,7 +32,7 @@ def RL(Mcalc=None, f_set=None, d_set=None, **kwargs):
     '''
 
     if Mcalc is None:
-        ErrorMsg = 'Data must be passed as an array which is mappabe to an Nx5 numpy array with columns [freq, e1, e2, mu1, mu2]'
+        ErrorMsg = 'Data must be passed as an array which is mappable to an Nx5 numpy array with columns [freq, e1, e2, mu1, mu2]'
         print(ErrorMsg)
         return ErrorMsg
 
@@ -190,3 +190,193 @@ def RL(Mcalc=None, f_set=None, d_set=None, **kwargs):
 
     return res
 
+def CARL(Mcalc=None, f_set=None, params="All", **kwargs):
+
+    '''
+    :param Mcalc: Permittivity and Permeability data
+    :param f_set: (start, end, [step]) tuple for frequency values in GHz
+                 - if given as list of len 3, results are interpolated
+                 - if given as list of len 2, results are data-derived
+                 with the calculation bound by the given start and
+                 end frequencies
+                 - if f_set is None, frequency is bound to input data
+    :param params: A set i.e. {} of text arguments for the parameters
+                   the user wants calculated. The available arguments
+                   are: {
+                   "tgde",          # dielectric loss tangent
+                   "tgdu",          # magnetic loss tangent
+                   "Qe",            # dielectric quality factor
+                   "Qu",            # magnetic quality factor
+                   "Qf",            # total quality factor
+                   "ReRefIndx",     # Refractive Index
+                   "ExtCoeff",      # Extinction Coeffecient
+                   "AtnuCnstNm",    # Attenuation Constant (in Np/m)
+                   "AtnuCnstdB",    # Attenuation Constant (in dB/m)
+                   "PhsCnst",       # Phase Constant
+                   "PhsVel",        # Phase Velocity
+                   "Res",           # Resistance
+                   "React",         # Reactance
+                   "Condt",         # Conductivity
+                   "Skd",           # Skin Depth
+                   "Eddy"           # Eddy Current Loss
+                   }
+                   If no set is passed, the default is to calculate everything.
+
+    :return: returns NxY data set of the requested parameters as columns 1 to Y with the input
+                  frequency values in column zero of length N.
+    '''
+
+    if Mcalc is None:
+        ErrorMsg = 'Data must be passed as an array which is mappable to an Nx5 numpy array with columns [freq, e1, e2, mu1, mu2]'
+        print(ErrorMsg)
+        return ErrorMsg
+
+    # allows for file location to be passed as the data variable.
+    if isinstance(Mcalc, str) is True:
+        if splitext(Mcalc)[1] == '.csv':
+            Mcalc = pd.read_csv(Mcalc, sep=',').to_numpy()
+
+        elif splitext(Mcalc)[1] == '.xlsx':
+            Mcalc = pd.read_excel(Mcalc).to_numpy()
+
+        # finds all rows in numpy array which aren't a part of the Nx5 data array expected.
+        # note, if the file contains more/less than 5 columns this fails as the 6th row is
+        # always filled with NaN. That being said, most instruments output a Nx5 data file.
+        x = []
+        for i in np.arange(Mcalc.shape[0]):
+            for k in np.arange(Mcalc.shape[1]):
+                if isinstance(Mcalc[i, k], (int, float)) is False or Mcalc[i, k] != Mcalc[i, k]:
+                    x.append(i)
+                    break
+
+        # removes non-data rows from input array to yield the data array
+        Mcalc = np.delete(Mcalc, x, axis=0)
+
+    # constants for later use
+    j = cmath.sqrt(-1)
+    c = 299792458
+    GHz = 10**9
+    mm = 10**(-3)
+    Z0 = 376.730313461
+    e0 = 8.854188 * 10 ** (-12)
+
+    # pass data to a numpy array
+    Mcalc = np.array(Mcalc)
+
+    # user option to use linear interpolation via a kwarg. Default is cubic spline.
+    # (e1, e2, mu1, mu2) = (Real Permittivity, Complex Permittivity, Real Permeability, Complex Permeability)
+    if 'interp' in kwargs and kwargs['interp'] is 'linear':
+
+        e1f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 1], dtype=np.float),
+            kind='linear', fill_value='extrapolate'
+        )
+
+        e2f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 2], dtype=np.float),
+            kind='linear', fill_value='extrapolate'
+        )
+
+        mu1f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 3], dtype=np.float),
+            kind='linear', fill_value='extrapolate'
+        )
+
+        mu2f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 4], dtype=np.float),
+            kind='linear', fill_value='extrapolate'
+        )
+
+    else:
+        e1f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 1], dtype=np.float),
+            kind='cubic', fill_value='extrapolate'
+        )
+
+        e2f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 2], dtype=np.float),
+            kind='cubic', fill_value='extrapolate'
+        )
+
+        mu1f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 3], dtype=np.float),
+            kind='cubic', fill_value='extrapolate'
+        )
+
+        mu2f = interp1d(
+            np.array(Mcalc[:, 0], dtype=np.float),
+            np.array(Mcalc[:, 4], dtype=np.float),
+            kind='cubic', fill_value='extrapolate'
+        )
+
+    np.errstate(divide='ignore')
+
+    if f_set is None:
+        f_vals=np.array([
+            m for m in Mcalc[:, 0]
+        ])
+
+    elif len(f_set) is 3:
+        f_vals=np.array([
+            m for m in np.arange(f_set[0], f_set[1]+f_set[2], f_set[2])
+        ])
+
+    elif len(f_set) is 2:
+        f_vals=np.array([
+            m for m in Mcalc[np.argwhere(np.abs(f_set[0]-Mcalc[:,0])<=Mcalc[1,0]-Mcalc[0,0])[0][0]:
+                             np.argwhere(np.abs(f_set[1]-Mcalc[:,0])<=Mcalc[1,0]-Mcalc[0,0])[0][0], 0]
+        ])
+
+    else:
+        ErrorMsg = 'Error in partitioning frequency values'
+        print(ErrorMsg)
+        return ErrorMsg
+
+    chars = {
+        "tgde": lambda f: e1f(f) / e2f(f),
+        "tgdu": lambda f: mu1f(f) / mu2f(f),
+        "Qe": lambda f: (e1f(f) / e2f(f))**-1,
+        "Qu": lambda f: (mu1f(f) / mu2f(f))**-1,
+        "Qf": lambda f: ((e1f(f) / e2f(f))+(mu1f(f) / mu2f(f)))**-1,
+        "ReRefIndx": lambda f: np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))).real,
+        "ExtCoeff": lambda f:  np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))).imag,
+        "AtnuCnstNm": lambda f: ((2*np.pi*f*GHz*np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))))*(c**-1)).real,
+        "AtnuCnstdB": lambda f: ((2*np.pi*f*GHz*np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))))*(c**-1)).real * 8.86588,
+        "PhsCnst": lambda f: ((2*np.pi*f*GHz*np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))))*(c**-1)).imag,
+        "PhsVel": lambda f: ((2 * np.pi * i * GHz) / ((2*np.pi*f*GHz*np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))))*(c**-1))).imag,
+        "Res": lambda f: (Z0 * np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f)))).real,
+        "React": lambda f: (Z0 * np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f)))).imag,
+        "Condt": lambda f: (2 * np.pi * f * GHz) * (e0 * e2f(f)),
+        "Skd": lambda f: 1000 / ((2*np.pi*f*GHz*np.sqrt((mu1f(f) - j * mu2f(f)) * (e1f(f) - j * e2f(f))))*(c**-1)).real,
+        "Eddy": lambda f: mu2f(f) / (mu1f(f) ** 2 * f)
+    }
+
+    if params is "All":
+        params = [
+            "tgde","tgdu","Qe","Qu","Qf",
+            "ReRefIndx","ExtCoeff",
+            "AtnuCnstNm","AtnuCnstdB",
+            "PhsCnst","PhsVel","Res",
+            "React","Condt","Skd","Eddy"
+        ]
+
+    Matrix, names = np.zeros((f_vals.shape[0],len(params)+1), dtype=float), ["frequency"]
+    Matrix[:,0] = f_vals[:]
+
+    for counter, param in enumerate(params, start=1):
+        Matrix[:,counter] = chars[param](f_vals[:])
+        names.append(param)
+
+    if 'as_dataframe' in kwargs and kwargs['as_dataframe'] is True:
+        panda_matrix = pd.DataFrame(Matrix)
+        panda_matrix.columns = names
+        return panda_matrix
+
+    return Matrix, names
