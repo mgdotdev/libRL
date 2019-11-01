785d6e72febc0e178eee6d2de37ba2766505b85d
1mikegrn <1mikegrn@gmail.com>
2019-11-01 15:42:12 -0500

    bug fix in CARL

--- example.py
+++ example.py
@@ -15,18 +15,18 @@ def main():
         path.dirname(__file__)
     ) + r'\test\test_data.xlsx'
 
-    # reflection_loss = libRL.RL(
-    #     Mcalc=import_file,
-    #     f_set=(2,18),
-    #     d_set=(0,20,1),
-    #     interp='cubic',
-    #     multiprocessing=True,
-    #     multicolumn=True,
-    #     as_dataframe=True,
-    #     quick_graph=True
-    # )
-    #
-    # print(reflection_loss)
+    reflection_loss = libRL.RL(
+        Mcalc=import_file,
+        f_set=(2,18),
+        d_set=(0,20,1),
+        interp='cubic',
+        multiprocessing=True,
+        multicolumn=True,
+        as_dataframe=True,
+        quick_graph=True
+    )
+
+    print(reflection_loss)
 
     characterization = libRL.CARL(
         Mcalc=import_file,
@@ -37,17 +37,17 @@ def main():
 
     print(characterization)
 
-    # band_analysis = libRL.BARF(
-    #     Mcalc=import_file,
-    #     f_set=(1,18),
-    #     d_set=(1,5,0.1),
-    #     m_set=[1,2,3,4,5],
-    #     thrs=-10,
-    #     as_dataframe=True,
-    #     quick_graph=True
-    # )
-    #
-    # print(band_analysis)
+    band_analysis = libRL.BARF(
+        Mcalc=import_file,
+        f_set=(1,18),
+        d_set=(1,5,0.1),
+        m_set=[1,2,3,4,5],
+        thrs=-10,
+        as_dataframe=True,
+        quick_graph=True
+    )
+
+    print(band_analysis)
 
 
 if __name__ == "__main__":
