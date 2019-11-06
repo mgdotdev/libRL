217dd2983921fc03b6c2ee6a1bbf416101866732
1mikegrn <1mikegrn@gmail.com>
2019-11-06 06:08:46 -0600

    delete cython_setup

--- libRL/cython_setup.py
+++
@@ -1,9 +0,0 @@
-# open anaconda prompt, cd to the location of the 'cython_setup.py' file, the run the following:
-# python cython_setup.py build_ext --inplace
-
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("cpfuncs.pyx")
-)
